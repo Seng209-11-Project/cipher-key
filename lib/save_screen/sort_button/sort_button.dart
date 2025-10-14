f4274f268693c63230c612f246231640785cfafa
Berat <kaplanberat@outlook.com>
2025-10-14 18:04:37 +0300

    added a lot of missing features, updated gradle,tidied up code

---
+++ lib/save_screen/sort_button/sort_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SortButton extends StatefulWidget {
+  const SortButton({super.key});
+
+  @override
+  State<SortButton> createState() => _SortButtonState();
+}
+
+class _SortButtonState extends State<SortButton> {
+  @override
+  Widget build(BuildContext context) {
+    String currentSortOption = 'Latest';
+    return PopupMenuButton<String>(
+      onSelected: (String newValue) {
+        setState(() {
+          currentSortOption = newValue;
+        });
+      },
+      color: Colors.white,
+      position: PopupMenuPosition.over,
+      offset: const Offset(0, -215),
+
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+        side: BorderSide(color: Colors.grey[300]!, width: 1.0),
+      ),
+      elevation: 4,
+
+      child: OutlinedButton(
+        onPressed: null,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.white,
+          disabledForegroundColor: Colors.black,
+          side: BorderSide(color: Colors.grey[300]!),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(Icons.swap_vert, size: 18),
+            const SizedBox(width: 8),
+            Text(currentSortOption),
+          ],
+        ),
+      ),
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+        const PopupMenuItem<String>(
+            value: 'Latest', child: Text('Latest First')),
+        const PopupMenuItem<String>(
+            value: 'Oldest', child: Text('Oldest First')),
+        const PopupMenuItem<String>(
+            value: 'By Password', child: Text('By Password (A-Z)')),
+        const PopupMenuItem<String>(
+            value: 'By Nickname', child: Text('By Nickname (A-Z)')),
+      ],
+    );
+  }
+}

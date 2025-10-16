5879b51b76dd5022308022f78d42a762436e4604
T4lha235 <talhakarci32@gmail.com>
2025-10-16 17:36:01 +0300

    homepage

--- password_generator/lib/utils/password_generator.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:math';
-
-String generateRandomPassword(int length) {
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_-+=<>?/[]{}|';
-  final random = Random();
-  return List.generate(length, (index) => chars[random.nextInt(chars.length)]).join();
-}

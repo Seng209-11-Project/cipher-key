a56b7b9df6c41e0700d70449cba9bf48779c0ae4
Berat <kaplanberat@outlook.com>
2025-10-21 20:13:06 +0300

    Updated Code added, app_theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:test_group_project/components/app_navigation_bar/app_navigation_bar.dart';
-import 'package:test_group_project/protein_bar.dart';
+import 'package:password_generator/app_navigation_bar/app_navigation_bar.dart';
+import 'package:password_generator/app_theme/theme_provider.dart';
+import 'package:password_generator/generate_screen/pages/password_generator_page.dart';
+import 'package:password_generator/save_screen/save_screen.dart';
+import 'package:provider/provider.dart';
+import 'app_theme/app_theme.dart';
+
+ValueNotifier<int> selectedIndex = ValueNotifier(0);
+bool isDark = false;
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,16 +22,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: AppNavigationBar(),
-        body: Center(
-          child: GestureDetector(
-            onTap: () {
-              proteinBarM(context, "Copied Password",icon: Icons.check_outlined);
-            },
-          ),
-        ),
-      )
+        theme: AppThemes.light,
+        darkTheme: AppThemes.dark,
+        themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+        home: ManagementWidget()
+    );
+  }
+}
+
+class ManagementWidget extends StatelessWidget {
+  final List<Widget> _pages = [
+    const PasswordGeneratorPage(),
+    const SaveScreen(),
+    //const SettingsPage(),
+  ];
+
+  ManagementWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ValueListenableBuilder(valueListenable: selectedIndex, builder: (_,value,context) {
+        return IndexedStack(
+          index: value,
+          children: _pages,
+        );
+      }),
+      bottomNavigationBar: const AppNavigationBar() // your current widget
     );
   }
 }
